cee22dac500d8e647e81a7c26600191e5074575b
robert <2137000099@utna.edu.mx>
2024-02-25 16:37:26 -0600

    creacion del modelo del la info, que recupere el objeto quejas

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,24 +1,32 @@
 import { Server } from 'azle';
-import express, { Request } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import {Queja} from './registro'
+let quejas : Queja[] = [{
+    id:1,
+    fecha: new Date(2024,1,23),
+    title: 'Exeso de baches',
+    descripcion: 'Debido a las lluvia y el paso del timepo, En frente mi calle existen muchos baches',
+    calle:'Teodoro Olivares Sur',
+    colonia:'Progreso Sur'
+}];  
 
-let db = {
-    hello: ''
-};
 
 export default Server(() => {
     const app = express();
-
+/*     let proximoId= req.body.id;
+    export function crearRegistro */
     app.use(express.json());
+    // Get mostrar las petiticiones
 
-    app.get('/db', (req, res) => {
-        res.json(db);
+    app.get('/petisiones', (req, res) => {
+        res.json(quejas);
     });
 
-    app.post('/db/update', (req: Request<any, any, typeof db>, res) => {
+    /* app.post('/db/update', (req: Request<any, any, typeof db>, res) => {
         db = req.body;
 
         res.json(db);
-    });
+    }); */
 
     app.use(express.static('/dist'));
 
---
+++ src/backend/registro.ts
@@ -0,0 +1,9 @@
+export type Queja={
+    id: number,
+    fecha: Date,
+    title:string,
+    descripcion:string,
+    calle:string,
+    colonia:string,
+
+}
